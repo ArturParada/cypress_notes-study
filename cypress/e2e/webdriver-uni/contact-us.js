d4df506433984202395b7808af114b30d42a18e3
Artur <paradaartur@gmail.com>
2023-04-13 19:53:11 +0200

    add PO

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -1,30 +1,30 @@
 /// <reference types="cypress" />
 import HomePage_PO from "../../support/PageObject/webdrivwer-uni/HomePage_PO"
+import ContactUs_PO from "../../support/PageObject/webdrivwer-uni/ContactUS_PO"
 
 describe("Test Contact Us form via WebdriverUni", () => {
+    const homePage_PO = new HomePage_PO
+    const contactUs_PO = new ContactUs_PO
+
     before(function () {
         cy.fixture('example').then((data) => {
-            // this.data = data
             globalThis.data = data
         })
     })
     beforeEach(() => {
-        const homePage_PO = new HomePage_PO
         homePage_PO.visitHomePage()
         homePage_PO.clickOnContactUsButton()
     })
     it("Should be able to submit a successful submission via contact us form", () => {
-        // cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
         cy.title().should('include', 'WebDriver | Contact Us');
         cy.url().should('include', 'contactus');
-        //cy.get('#contact-us').click({force: true})
-        cy.contactFormSubmission(Cypress.env("first_name"), data.last_name, data.email, "How can I learn Cypress?", 'h1', 'Thank You for your Message!')
+        contactUs_PO.contactFormSubmission(Cypress.env("first_name"), data.last_name, data.email, "How can I learn Cypress?", 'h1', 'Thank You for your Message!')
 
     });
 
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-        // cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
-        cy.contactFormSubmission(data.first_name, data.last_name, " ", "How can I learn Cypress?", 'body', 'Error: Invalid email address')
+        contactUs_PO.contactFormSubmission(data.first_name, data.last_name, " ", "How can I learn Cypress?", 'body', 'Error: Invalid email address')
+
     });
 })
---
+++ cypress/support/PageObject/webdrivwer-uni/ContactUS_PO.js
@@ -0,0 +1,13 @@
+class ContactUs_PO {
+    contactFormSubmission(firstName, lastName, email, comments, $selector, textToLocate) {
+        cy.get('[name="first_name"]').type(firstName);
+        cy.get('[name="last_name"]').type(lastName);
+        cy.get('[name="email"]').type(email)
+        cy.get('textarea.feedback-input').type(comments)
+        cy.get('[type="submit"]').click();
+        cy.get($selector).contains(textToLocate)
+    }
+
+}
+
+export default ContactUs_PO
