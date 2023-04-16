bc54650c3475b1f9aae3c53a1c1a2f22dc040596
Artur <paradaartur@gmail.com>
2023-04-16 13:38:00 +0200

    change

--- cypress/support/PageObject/automation-test-store/AutoStoreHomePage_PO.js
+++ cypress/support/PageObject/automation-test-store/AutoStoreHomePage_PO.js
@@ -1,14 +1,10 @@
 class AutoStoreHomePage {
-    homPageSeletor() {
-        const test = cy.get("a[href*='product/category&path=']")
-        return
-    }
 
-    acessHomePage() {
+    accessHomePage() {
         cy.visit("https://automationteststore.com/");
     }
     clickOnHaireCareLink() {
-        this.homPageSeletor.contains("Hair Care").click();
+        cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
     }
 
 }
